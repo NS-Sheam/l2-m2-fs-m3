2f0fb6025228af72109cd1422220a65c9b9ea6eb
NS-Sheam <123sheamfeni@gmail.com>
2024-01-13 18:31:50 +0600

    get api done

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -1,9 +1,18 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:3000",
   }),
-  endpoints: () => ({}),
+  endpoints: (builder) => ({
+    getTodos: builder.query({
+      query: () => ({
+        url: "/todos",
+        method: "GET",
+      }),
+    }),
+  }),
 });
+
+export const { useGetTodosQuery } = baseApi;
